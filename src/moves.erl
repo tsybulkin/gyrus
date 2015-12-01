7f5f369076b7619294c763389b77d58f751a455f
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-12-01 07:15:58 +0200

    a little change in moves selection

--- src/moves.erl
+++ src/moves.erl
@@ -90,7 +90,7 @@ t() ->
 get_good_moves(Me,Lines) ->
 	Moves = lists:foldl(fun(Line,Acc) -> rate_line(Me,Line)++Acc end,[],Lines), 
 	Sorted = lists:sort(fun({_,A},{_,B})-> A>B end, group(lists:sort(Moves))),
-	[{_,MaxRate}|_] = Sorted, Thr = 0.7*MaxRate,
+	[{_,MaxRate}|_] = Sorted, Thr = 0.55*MaxRate,
 	Best = pick_best(lists:filter(fun({_,R})-> R>=Thr end,Sorted),Thr+1),
 	shuffle(Best).
 
